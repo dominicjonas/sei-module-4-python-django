da400cd64e5b2ecda30ed281cda1e9f5074615db
Dominic Fergal <dominicfergal@Dominics-MacBook-Air.local>
2021-10-26 12:39:59 +0100

    expanded model

--- composers/models.py
+++ composers/models.py
@@ -5,6 +5,8 @@ class Composer(models.Model):
     name = models.CharField(max_length=50)
     period = models.CharField(max_length=50)
     cover_image = models.CharField(max_length=200)
+    born = models.CharField(max_length=50)
+    died = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.name} by {self.period} ({self.id})"
